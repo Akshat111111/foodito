ef97d14cedb31bca8824ee7fdfed41b1263e2c93
Akshat Sharma <111536616+Akshat111111@users.noreply.github.com>
2023-05-13 15:08:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import {foodItem} from './fooditem.js'
 
 function displayItems(){
-    var biryani= document.getElementById('Deserts');
-    var paneer=  document.getElementById('Starters');;
+    var biryani= document.getElementById('biryani');
+    var paneer=  document.getElementById('paneer');;
     var chicken=  document.getElementById('chicken');
     var vegetable=  document.getElementById('vegetable');
     var chinese=  document.getElementById('chinese');

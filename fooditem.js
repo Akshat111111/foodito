e19823148450b6a73ee2e6833ea28eaacbc42a94
Akshat Sharma <111536616+Akshat111111@users.noreply.github.com>
2023-05-13 15:17:19 +0530

    Update fooditem.js

--- fooditem.js
+++ fooditem.js
@@ -1,14 +1,14 @@
 const foodItem= [
     {
-    /*id: 1,
+    id: 1,
     name: 'Ambur Biryani',
     category : 'biryani',
     rating : 4.3,
     price: 13,
     img: 'images/biryani/Ambur-Chicken-Biryani.jpg',
     quantity: 1
-}, */
-{
+}, 
+/*{
     id: 2,
     name: 'Hyderabadi Biryani',
     category : 'biryani',
@@ -16,7 +16,7 @@ const foodItem= [
     price: 15,
     img: 'images/biryani/Chicken-Biryani-hyd.jpg',
     quantity: 1
-},
+},*/
 {
     id: 3,
     name: 'Egg Biryani',
